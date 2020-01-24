800bfa2efb9d045985d554e515ef00655bd10082
ragnta <ragnta@gmail.com>
2020-01-24 08:27:12 +0100

    add python comments

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,11 @@ from app import app
 from flask import json, request
 from flask_cors import CORS, cross_origin
 from .reader import Reader
-
+# this file contains every route. It defines a REST interface for communicate with the client (eg.: browser)
+# cors see https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 cors = CORS(app)
 
+# you can get all data by http://<ipadress>:<port:default:5000>/availabledata
 @cross_origin()
 @app.route('/availabledata',  methods=['GET'])
 def get_availabledata():
@@ -18,6 +20,7 @@ def post_new_data():
     print(f'\t{valueFromRequest}.')
     return json.dumps({"success": True})
 
+# you can get a data by http://<ipadress>:<port:default:5000>/getdatabyid?id=<id>
 @cross_origin()
 @app.route('/getdatabyid',  methods=['GET'])
 def get_data_by_id():
