4c29460299bd8e4441771f5776d699d7a05b9e04
vagrant <vagrant@vagrant.vm>
2020-02-01 21:27:49 +0000

    add insert medicines

--- simple/databaseinit.py
+++ simple/databaseinit.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
- 
+from app import MadDao
+from app import Reader
  
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
@@ -10,9 +11,7 @@ def create_connection(db_file):
         print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+    return conn
 
 def create_Medicine(conn, medicine):
     """
@@ -50,6 +49,19 @@ def create_Dosage(conn, dosage):
     cur.execute(sql, dosage)
     return cur.lastrowid
 
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
 def create_Resident_Medicine(conn, resident_medicine):
     """
         Create resident_medicine:
@@ -62,5 +74,24 @@ def create_Resident_Medicine(conn, resident_medicine):
     cur.execute(sql, resident_medicine)
     return cur.lastrowid
 
+def main():
+    conn = create_connection(r"./pythonsqlite.db")
+        ##id,barcode, shortBar, name, dosage, pieces
+    sql_create_medicine_table = """ CREATE TABLE IF NOT EXISTS Medicine (
+                                        id text PRIMARY KEY,
+                                        barcode text NOT NULL,
+                                        shortBar text,
+                                        name text,
+                                        dosage text,
+                                        pieces text
+                                    ); """
+
+    create_table(conn, sql_create_medicine_table)
+    dao = MadDao(Reader())
+    medicines = dao.getAllMadicines()
+    ## TODO convert medicines to string object or something
+    [create_Medicine(conn, x) for x in medicines]
+
+
 if __name__ == '__main__':
-    create_connection(r"./pythonsqlite.db")
+    main()
